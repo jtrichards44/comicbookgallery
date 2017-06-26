0b7c4e43880ca48804f7b5a246be5e0f1ca38053
christi youngs <christianarlawrence@gmail.com>
2017-06-26 18:11:34 -0400

    UpdatedControllerToUseRepository

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,20 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hour of Doctor Octopus' life and his one, last great act of revenge! Even if Spider-man survives...<strong> will Peter Parker?</strong></p>",
-                Artists = new Artists[]
-                {
-                    new Artists() { Name = "Dan Slott", Role = "Script"},
-                    new Artists() { Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artists() { Name = "Victor Olazaba", Role = "Inks"},
-                    new Artists() { Name = "Edgar Delgado", Role = "Colors"},
-                    new Artists() { Name = "Chris Eliopoulos", Role = "Letters"},
-                }
-                 
-            };
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
      
             return View(comicBook);
         }
